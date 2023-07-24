a42796902b73c6a73cc5c65b340a9b063bcc6845
Russ <russ3750@gmail.com>
2023-07-24 13:29:57 -0500

    Exercise 1.2

--- JS/scripts.js
+++ JS/scripts.js
@@ -1,4 +1,22 @@
-// Path: JS/scripts.js
-alert('Hello World');
-favoriteFood = 'Tuna';
-document.write(favoriteFood);
+let pokemonList = [];
+
+// Bulbasaur
+pokemonList.push({
+	name: "Bulbasaur",
+	height: 7,
+	types: ["grass", "poison"],
+});
+
+// Charmander
+pokemonList.push({
+	name: "Charmander",
+	height: 6,
+	types: ["fire"],
+});
+
+// Squirtle
+pokemonList.push({
+	name: "Squirtle",
+	height: 5,
+	types: ["water"],
+});
