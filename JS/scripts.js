fc21839f2b4f6a58e8e28ecb586738696084f871
Russ <russ3750@gmail.com>
2023-07-24 21:34:55 -0500

    Exercise 1.3

--- JS/scripts.js
+++ JS/scripts.js
@@ -1,22 +1,35 @@
-let pokemonList = [];
+//Start
+let pokemonList = [
+	{
+		name: "Bulbasaur",
+		height: 7,
+		types: ["grass", "poison"],
+	},
+	{
+		name: "Charmander",
+		height: 6,
+		types: ["fire"],
+	},
+	{
+		name: "Squirtle",
+		height: 5,
+		types: ["water"],
+	},
+];
 
-// Bulbasaur
-pokemonList.push({
-	name: "Bulbasaur",
-	height: 7,
-	types: ["grass", "poison"],
-});
+for (let i = 0; i < pokemonList.length; i++) {
+	let pokemonName = pokemonList[i].name;
+	let pokemonHeight = pokemonList[i].height;
 
-// Charmander
-pokemonList.push({
-	name: "Charmander",
-	height: 6,
-	types: ["fire"],
-});
+	// Initializes output message with name and height
+	let outputMessage = pokemonName + " (height: " + pokemonHeight + ")";
 
-// Squirtle
-pokemonList.push({
-	name: "Squirtle",
-	height: 5,
-	types: ["water"],
-});
+	// Conditional check for if the Pokemon is big
+	if (pokemonHeight > 6) {
+		// If the Pokemon is big, add special message to output
+		outputMessage += " - Wow, that’s big!";
+	}
+
+	// Whether the Pokemon is big or not, write the output message to the web page
+	document.write(outputMessage + "<br>");
+}
