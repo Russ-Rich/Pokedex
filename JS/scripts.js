8ab17a1c83f198c7bd4e8aa2eaf141bb1a56b8e6
Russ <russ3750@gmail.com>
2023-07-25 17:24:23 -0500

    Exercise 1.5 Part 2

--- JS/scripts.js
+++ JS/scripts.js
@@ -1,37 +1,61 @@
-//Start
-let pokemonList = [
-	{
-		name: "Bulbasaur",
-		height: 7,
-		types: ["grass", "poison"],
-	},
-	{
-		name: "Charmander",
-		height: 6,
-		types: ["fire"],
-	},
-	{
-		name: "Squirtle",
-		height: 5,
-		types: ["water"],
-	},
-];
+// Pokemon List
+let pokemonRepository = (function () {
+	// Initialize list of pokemon
+	let pokemonList = [
+		{ name: "Bulbasaur", height: 7, types: ["grass", "poison"] },
+		{ name: "Charizard", height: 1.7, types: ["fire", "flying"] },
+		{ name: "Pikachu", height: 0.4, types: ["electric"] },
+	];
 
-// New forEach() loop
-pokemonList.forEach(function(pokemon) {
-	let pokemonName = pokemonList.name;
-	let pokemonHeight = pokemonList.height;
-
-	// Initializes output message with name and height
-	let outputMessage = pokemonName + " (height: " + pokemonHeight + ")";
-
-	// Conditional check for if the Pokemon is big
-	if (pokemonHeight > 6) {
-		// If the Pokemon is big, add special message to output
-		outputMessage += " - Wow, that’s big!";
+	// Function to return all pokemon in list
+	function getAll() {
+		return pokemonList;
 	}
 
-	// Whether the Pokemon is big or not, write the output message to the web page
-	document.write(outputMessage + "<br>");
-});
+	// Function to add a new pokemon to list with validation
+	function add(pokemon) {
+		let keys = ["name", "height", "types"];
+		let isValidPokemon = keys.every((key) => key in pokemon);
 
+		// Check that pokemon is an object and it has all keys and correct types for values
+		if (
+			typeof pokemon === "object" &&
+			isValidPokemon &&
+			typeof pokemon.name === "string" &&
+			typeof pokemon.height === "number" &&
+			Array.isArray(pokemon.types)
+		) {
+			// If the pokemon data is valid, add it to the list
+			pokemonList.push(pokemon);
+		} else {
+			// If the data is NOT valid, log error message
+			console.log("Invalid Pokemon data");
+		}
+	}
+
+	// Function to find a pokemon by name
+	function findByName(name) {
+		return pokemonList.filter((pokemon) => pokemon.name === name);
+	}
+
+	// Return our functions for the pokemon repository
+	return {
+		getAll: getAll,
+		add: add,
+		findByName: findByName,
+	};
+})();
+
+// Displays pokemon on webpage
+pokemonRepository.getAll().forEach((pokemon) => {
+	// Output string with name and height
+	let output = `${pokemon.name} (height: ${pokemon.height})`;
+
+	// If the pokemon is taller than 1.0, add a comment to the output string
+	if (pokemon.height > 1.0) {
+		output += " - Wow, that's big!";
+	}
+
+	// Write the output string to the webpage
+	document.write(`<p>${output}</p>`);
+});
