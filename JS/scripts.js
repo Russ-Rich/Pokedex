d24e2d165731d70b87c6f2966114ed66ec8bb584
Russ <russ3750@gmail.com>
2023-07-26 23:08:27 -0500

    Exercise 1.6

--- JS/scripts.js
+++ JS/scripts.js
@@ -1,61 +1,61 @@
-// Pokemon List
+// Creating a Pokemon Repository inside an IIFE
 let pokemonRepository = (function () {
-	// Initialize list of pokemon
+	// Pokemon list array
 	let pokemonList = [
 		{ name: "Bulbasaur", height: 7, types: ["grass", "poison"] },
-		{ name: "Charizard", height: 1.7, types: ["fire", "flying"] },
-		{ name: "Pikachu", height: 0.4, types: ["electric"] },
+		{ name: "Charmander", height: 6, types: ["fire"] },
+		{ name: "Charizard", height: 17, types: ["fire", "flying"] },
+		{ name: "Pikachu", height: 4, types: ["electric"] },
+		{ name: "Squirtle", height: 5, types: ["water"] },
+		{ name: "Jigglypuff", height: 5, types: ["normal", "fairy"] },
+		{ name: "Mewtwo", height: 20, types: ["psychic"] },
+		{ name: "Mew", height: 4, types: ["psychic"] },
+		{ name: "Gengar", height: 15, types: ["ghost", "poison"] },
+		{ name: "Gyarados", height: 21, types: ["water", "flying"] },
+		{ name: "Dragonite", height: 22, types: ["dragon", "flying"] },
+		{ name: "Eevee", height: 3, types: ["normal"] },
+
 	];
 
-	// Function to return all pokemon in list
+	// Function to return all Pokemon
 	function getAll() {
 		return pokemonList;
 	}
 
-	// Function to add a new pokemon to list with validation
+	// Function to add a new Pokemon
 	function add(pokemon) {
-		let keys = ["name", "height", "types"];
-		let isValidPokemon = keys.every((key) => key in pokemon);
-
-		// Check that pokemon is an object and it has all keys and correct types for values
-		if (
-			typeof pokemon === "object" &&
-			isValidPokemon &&
-			typeof pokemon.name === "string" &&
-			typeof pokemon.height === "number" &&
-			Array.isArray(pokemon.types)
-		) {
-			// If the pokemon data is valid, add it to the list
-			pokemonList.push(pokemon);
-		} else {
-			// If the data is NOT valid, log error message
-			console.log("Invalid Pokemon data");
-		}
+		pokemonList.push(pokemon);
 	}
 
-	// Function to find a pokemon by name
-	function findByName(name) {
-		return pokemonList.filter((pokemon) => pokemon.name === name);
+	// Function to add a list item
+	function addListItem(pokemon) {
+		let pokemonList = document.querySelector(".pokemon-list");
+		let listItem = document.createElement("li");
+		let button = document.createElement("button");
+		button.innerText = pokemon.name;
+		button.classList.add("button-class");
+		listItem.appendChild(button);
+		pokemonList.appendChild(listItem);
+		button.addEventListener("click", function () {
+			showDetails(pokemon);
+		});
 	}
 
-	// Return our functions for the pokemon repository
+	// Function to show Pokemon details
+	function showDetails(pokemon) {
+		console.log(pokemon);
+	}
+
+	// Returning public functions
 	return {
-		getAll: getAll,
 		add: add,
-		findByName: findByName,
+		getAll: getAll,
+		addListItem: addListItem,
+		showDetails: showDetails,
 	};
 })();
 
-// Displays pokemon on webpage
-pokemonRepository.getAll().forEach((pokemon) => {
-	// Output string with name and height
-	let output = `${pokemon.name} (height: ${pokemon.height})`;
-
-	// If the pokemon is taller than 1.0, add a comment to the output string
-	if (pokemon.height > 1.0) {
-		output += " - Wow, that's big!";
-	}
-
-	// Write the output string to the webpage
-	document.write(`<p>${output}</p>`);
+// Looping each Pokemon and creating a list item
+pokemonRepository.getAll().forEach(function (pokemon) {
+	pokemonRepository.addListItem(pokemon);
 });
