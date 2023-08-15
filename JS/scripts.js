7f61fc23308a72e7503bbed5576dc59a1f33f567
Russ <russ3750@gmail.com>
2023-08-15 13:19:30 -0500

    Exercise 1.11

--- JS/scripts.js
+++ JS/scripts.js
@@ -1,9 +1,13 @@
 let pokemonRepository = (function () {
 	let pokemonList = [];
-	let apiUrl = "https://pokeapi.co/api/v2/pokemon/?limit=150";
+	const apiUrl = "https://pokeapi.co/api/v2/pokemon/?limit=150";
 
 	function add(pokemon) {
-		pokemonList.push(pokemon);
+		if (typeof pokemon === "object" && "name" in pokemon) {
+			pokemonList.push(pokemon);
+		} else {
+			console.error("Invalid Pokémon data");
+		}
 	}
 
 	function getAll() {
@@ -12,63 +16,55 @@ let pokemonRepository = (function () {
 
 	function addListItem(pokemon) {
 		let pokemonList = document.querySelector(".pokemon-list");
-		let listItem = document.createElement("li");
-		listItem.classList.add("list-group-item");
+		let column = document.createElement("div");
+		column.classList.add("col-lg-3", "col-md-4", "col-sm-6", "mb-4"); // Change column size as needed
+		let card = document.createElement("div");
+		card.classList.add("card");
 		let button = document.createElement("button");
 		button.innerText = pokemon.name;
 		button.classList.add("btn", "btn-primary");
 		button.setAttribute("data-toggle", "modal");
 		button.setAttribute("data-target", "#pokemonModal");
-		listItem.appendChild(button);
-		pokemonList.appendChild(listItem);
+		card.appendChild(button);
+		column.appendChild(card);
+		pokemonList.appendChild(column);
 		button.addEventListener("click", function () {
 			showDetails(pokemon);
 		});
 	}
 
-	function loadList() {
+
+	async function loadList() {
 		showLoadingMessage();
-		return fetch(apiUrl)
-			.then(function (response) {
-				return response.json();
-			})
-			.then(function (json) {
-				json.results.forEach(function (item) {
-					let pokemon = {
-						name: item.name,
-						detailsUrl: item.url,
-					};
-					add(pokemon);
-				});
-				hideLoadingMessage();
-			})
-			.catch(function (e) {
-				console.error(e);
-				hideLoadingMessage();
+		try {
+			const response = await fetch(apiUrl);
+			const json = await response.json();
+			json.results.forEach((item) => {
+				add({ name: item.name, detailsUrl: item.url });
 			});
+		} catch (e) {
+			console.error(e);
+		}
+		hideLoadingMessage();
 	}
 
-	function loadDetails(item) {
+	async function loadDetails(item) {
 		let url = item.detailsUrl;
 		showLoadingMessage();
-		return fetch(url)
-			.then(function (response) {
-				return response.json();
-			})
-			.then(function (details) {
-				item.imageUrl = details.sprites.front_default;
-				item.height = details.height;
-				item.types = details.types;
-				hideLoadingMessage();
-			})
-			.catch(function (e) {
-				console.error(e);
-				hideLoadingMessage();
-			});
+		try {
+			const response = await fetch(url);
+			const details = await response.json();
+			item.imageUrl = details.sprites.front_default;
+			item.height = details.height;
+			item.types = details.types;
+		} catch (e) {
+			console.error(e);
+		}
+		hideLoadingMessage();
 	}
 
 	function showDetails(pokemon) {
-		loadDetails(pokemon).then(function () {
+		loadDetails(pokemon).then(() => {
 			showModal(pokemon);
 		});
 	}
@@ -76,24 +72,21 @@ let pokemonRepository = (function () {
 	function showModal(pokemon) {
 		let modalBody = document.querySelector(".modal-body");
 		modalBody.innerHTML = "";
-
-		let titleElement = document.createElement("h1");
-		titleElement.innerText = pokemon.name;
-
-		let contentElement = document.createElement("p");
-		contentElement.innerText = "Height: " + pokemon.height;
-
-		let imageElement = document.createElement("img");
+		modalBody.appendChild(createElement("h1", pokemon.name));
+		modalBody.appendChild(createElement("p", "Height: " + pokemon.height));
+		let imageElement = createElement("img");
 		imageElement.src = pokemon.imageUrl;
-
-		modalBody.appendChild(titleElement);
-		modalBody.appendChild(contentElement);
 		modalBody.appendChild(imageElement);
 	}
 
+	function createElement(tag, text) {
+		let element = document.createElement(tag);
+		if (text) element.innerText = text;
+		return element;
+	}
+
 	function showLoadingMessage() {
-		let loadingMessage = document.createElement("p");
-		loadingMessage.innerText = "Loading data from the Pokémon API...";
+		let loadingMessage = createElement("p", "Loading data from the Pokémon API...");
 		document.body.appendChild(loadingMessage);
 	}
 
@@ -102,17 +95,105 @@ let pokemonRepository = (function () {
 	}
 
 	return {
-		add: add,
-		getAll: getAll,
-		addListItem: addListItem,
-		showDetails: showDetails,
-		loadList: loadList,
-		loadDetails: loadDetails,
+		add,
+		getAll,
+		addListItem,
+		showDetails,
+		loadList,
+		loadDetails,
 	};
 })();
 
-pokemonRepository.loadList().then(function () {
-	pokemonRepository.getAll().forEach(function (pokemon) {
+pokemonRepository.loadList().then(() => {
+	pokemonRepository.getAll().forEach((pokemon) => {
 		pokemonRepository.addListItem(pokemon);
 	});
 });
+document.getElementById("searchBar").addEventListener("keyup", (e) => {
+	let searchString = e.target.value.toLowerCase();
+	let filteredPokemon = pokemonRepository.getAll().filter((pokemon) => {
+		return pokemon.name.toLowerCase().includes(searchString);
+	});
+	document.querySelector(".pokemon-list").innerHTML = "";
+	filteredPokemon.forEach((pokemon) => {
+		pokemonRepository.addListItem(pokemon);
+	});
+});
+
+/// Function to populate the grid with Pokémon thumbnails
+function populateGrid(pokemonList) {
+  // Getting the grid container element from the DOM
+  const gridContainer = document.getElementById('grid-container');
+
+  // Iterating through the Pokémon list
+  pokemonList.forEach(pokemon => {
+    // Fetching details for each Pokémon from their specific URL
+    fetch(pokemon.url)
+      .then(response => response.json())
+      .then(details => {
+        // Creating a grid box for each Pokémon
+        const gridBox = document.createElement('div');
+        gridBox.className = 'grid-box';
+
+        // Creating an image element for the Pokémon thumbnail
+        const thumbnail = document.createElement('img');
+        thumbnail.src = details.sprites.front_default; // Getting the image from the details
+        thumbnail.alt = details.name;
+        thumbnail.className = 'thumbnail';
+
+        // Creating a heading element for the Pokémon name
+        const name = document.createElement('h3');
+        name.textContent = details.name;
+
+        // Appending the thumbnail and name to the grid box
+        gridBox.appendChild(thumbnail);
+        gridBox.appendChild(name);
+
+        // Appending the grid box to the grid container
+        gridContainer.appendChild(gridBox);
+      })
+      .catch(error => console.error(`An error occurred fetching ${pokemon.name}:`, error));
+  });
+}
+
+// Fetching the initial Pokémon data from the API
+fetch('https://pokeapi.co/api/v2/pokemon/?limit=150')
+  .then(response => response.json())
+  .then(data => populateGrid(data.results)) // Calling the populateGrid function with the results
+  .catch(error => console.error('An error occurred:', error));
+
+// Function to filter the grid based on the search input
+function filterGrid() 
+{
+	// Getting the search input and converting it to lowercase
+	const searchInput = document.getElementById('search-input').value.toLowerCase();
+
+	// Getting all the grid boxes
+	const gridBoxes = document.querySelectorAll('.grid-box');
+
+	// Iterating through the grid boxes
+	gridBoxes.forEach(gridBox => {
+		// Getting the name of the Pokémon from the heading element
+		const name = gridBox.querySelector('h3').textContent.toLowerCase();
+
+		// Checking if the name includes the search input
+		if (name.includes(searchInput)) {
+			// If it does, display the grid box
+			gridBox.style.display = 'block';
+		} else {
+			// If it doesn't, hide the grid box
+			gridBox.style.display = 'none';
+		}
+	});
+}
+
+function returnToHome() {
+	document.getElementById("searchBar").value = "";
+	document.querySelector(".pokemon-list").innerHTML = "";
+	pokemonRepository.getAll().forEach((pokemon) => {
+		pokemonRepository.addListItem(pokemon);
+	});
+}
+
+document.getElementById("homeButton").addEventListener("click", returnToHome);
+
---
+++ README.md
@@ -0,0 +1,61 @@
+# Pokémon List Web Application
+
+## Project Description
+
+This project is a simple web application that displays a list of Pokémon, allowing users to browse, search, and view detailed information about each Pokémon. It's designed to be user-friendly and offers a rich interface to engage Pokémon enthusiasts.
+
+### Reflection on the Work
+
+- **Role and Tasks**: As the sole developer,  I was responsible for designing the architecture, implementing the core functionality, and styling the user interface. I faced challenges such as integrating the Pokémon API and creating a responsive layout.
+
+- **Decisions and Consequences**: I chose to use Bootstrap for the styling to speed up the development process. This decision led to a consistent design but limited some customization options. I also decided to use vanilla JavaScript instead of a framework like React, making the application lightweight but requiring more manual management of the DOM.
+
+- **What Would Be Done Differently**: If given the opportunity, I might choose a modern front-end framework like React for more efficient state management and component reusability. Additionally, I'd invest more time in user experience (UX) design to enhance user interaction.
+
+- **Lessons Learned**: This project taught me the importance of planning, the effectiveness of modular coding, and the value of user feedback. Balancing design and functionality was a continuous learning experience..
+
+### Technologies Used
+
+- **HTML**: Structured the content.
+- **CSS & Bootstrap**: Styled the application.
+- **JavaScript & jQuery**: Managed interactivity.
+
+### Other Relevant Materials
+
+- **User Flows & User Stories**: Created user flows and stories to guide the development of the navigation and interaction design, ensuring a seamless user experience.
+- **Kanban Board**: Utilized a Kanban board for task management, enabling smooth collaboration with other team members and efficient tracking of project progress.
+
+## Getting Started
+
+### Prerequisites
+
+- Web Browser (e.g., Chrome, Firefox)
+- Internet connection
+
+### Installation
+
+1. Clone the repository from GitHub.
+2. Open `index.html` in your browser.
+
+## Contributing
+
+Contributions are welcome! Please follow these steps:
+
+1. Fork the project on GitHub.
+2. Create a new branch.
+3. Commit your changes.
+4. Open a Pull Request.
+
+For major changes, please open an issue first to discuss what you'd like to change.
+
+## License
+
+This project is licensed under the MIT License. See the `LICENSE.md` file for details.
+
+## Contact Information
+
+- **GitHub**: [Russ-Rich](https://github.com/Russ-Rich)
+
+Feel free to reach out if you have questions, ideas, or would like to collaborate!
+
+---
